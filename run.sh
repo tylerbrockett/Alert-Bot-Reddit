93cc8bea8516d3f5b27bde272480480f9fbd5dd2
Tyler Brockett <tylerbrockett@gmail.com>
2015-12-17 22:33:12 -0700

    changed run scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-./run_bot.sh  &  
-./run_monitor.sh
+project_root=`dirname $0`
+
+bot=${project_root%%/}/bot/bot.py
+# gnome-terminal --command "python $bot"
+
+# XTERM is for other systems
+xterm -fa 'Monospace' -fs 10 -hold -title "/r/buildapcsales bot" -e python $bot
