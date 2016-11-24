303351b12b1627d44e879c3858aa3fcd3386b723
Tyler Brockett <tylerbrockett@gmail.com>
2016-11-24 00:52:48 -0700

    Removed files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-project_root=`dirname $0`
-
-bot=${project_root%%/}/bot/bot.py
-# gnome-terminal --command "python $bot"
-
-# XTERM is for other systems
-xterm -fa 'Monospace' -fs 10 -hold -title "/r/buildapcsales bot" -e python $bot
