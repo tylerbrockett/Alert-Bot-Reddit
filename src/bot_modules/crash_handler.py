9e2c6897348fde3b2a0a29ad7014ce1cf15da130
Tyler Brockett <tylerbrockett@gmail.com>
2020-03-07 19:11:04 -0700

    Update "last edited" header

--- src/bot_modules/crash_handler.py
+++ src/bot_modules/crash_handler.py
@@ -4,7 +4,7 @@ Author:             Tyler Brockett
 Username:           /u/tylerbrockett
 Description:        Alert Bot (Formerly sales__bot)
 Date Created:       11/13/2015
-Date Last Edited:   04/02/2017
+Date Last Edited:   03/07/2020
 Version:            v2.0
 ==========================================
 """
